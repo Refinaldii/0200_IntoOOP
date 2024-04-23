a9523d938f8ad0abc0ca04753b17e6056ca96c5c
Refinaldii <refinaldi9786@gmail.com>
2024-04-23 09:49:58 +0700

    membuat fungsi output

--- 0200_IntoOOP.cpp
+++ 0200_IntoOOP.cpp
@@ -6,4 +6,10 @@ public: // aksess modifer
 	string nama;
 	int umur;
 	string jurusan;
+
+	void output() {
+		cout << "Nama: " << nama << endl;
+		cout << "Umur: " << umur << endl;
+		cout << "Jurusan: " << jurusan << endl;
+	}
 };
